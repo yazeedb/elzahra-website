7f06d124375a49500474bae89ae82195c7b1a5bf
Yazeed Bzadough <ybzadough@gmail.com>
2019-11-30 20:46:11 -0500

    Squashed commit of the following:
    commit 8d3e3faf2bde06b8ec5cc48129e1fded942b115b
    Author: Yazeed Bzadough <ybzadough@gmail.com>
    Date:   Sat Nov 30 20:46:03 2019 -0500
    
        add basic About page

---
+++ src/pages/about.js
@@ -0,0 +1,131 @@
+import React from "react"
+import { Link, graphql, useStaticQuery } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { Typography, makeStyles } from "@material-ui/core"
+import { getTitleStyles } from "."
+
+const useStyles = makeStyles(theme => {
+  return {
+    title: {
+      ...getTitleStyles(theme),
+      marginTop: theme.spacing(8),
+    },
+    "@global": {
+      img: {
+        width: "100%",
+      },
+      figure: {
+        margin: 0,
+      },
+    },
+  }
+})
+
+const AboutPage = () => {
+  const classes = useStyles()
+
+  const aboutUsContent = useStaticQuery(graphql`
+    query AboutUsContent {
+      aboutTheImam: wordpressPost(
+        categories: {
+          elemMatch: {
+            parent_element: { name: { eq: "About" } }
+            name: { eq: "About the Imam" }
+          }
+        }
+      ) {
+        id
+        title
+        content
+      }
+      history: wordpressPost(
+        categories: {
+          elemMatch: {
+            parent_element: { name: { eq: "About" } }
+            name: { eq: "History" }
+          }
+        }
+      ) {
+        id
+        title
+        content
+      }
+      mission: wordpressPost(
+        categories: {
+          elemMatch: {
+            parent_element: { name: { eq: "About" } }
+            name: { eq: "Mission" }
+          }
+        }
+      ) {
+        id
+        title
+        content
+      }
+      objectives: wordpressPost(
+        categories: {
+          elemMatch: {
+            parent_element: { name: { eq: "About" } }
+            name: { eq: "Objectives" }
+          }
+        }
+      ) {
+        id
+        title
+        content
+      }
+      boardMembers: wordpressPost(
+        categories: {
+          elemMatch: {
+            parent_element: { name: { eq: "About" } }
+            name: { eq: "Board Members" }
+          }
+        }
+      ) {
+        id
+        title
+        content
+      }
+    }
+  `)
+
+  const {
+    mission,
+    objectives,
+    history,
+    aboutTheImam,
+    boardMembers,
+  } = aboutUsContent
+
+  return (
+    <Layout>
+      <SEO title="About Us" />
+
+      {Object.keys({
+        mission,
+        objectives,
+        history,
+        aboutTheImam,
+        boardMembers,
+      }).map(key => {
+        const { id, title, content } = aboutUsContent[key]
+
+        return (
+          <div key={id}>
+            <Typography variant="h4" className={classes.title}>
+              {title}
+            </Typography>
+
+            <Typography
+              variant="body1"
+              dangerouslySetInnerHTML={{ __html: content }}
+            />
+          </div>
+        )
+      })}
+    </Layout>
+  )
+}
+
+export default AboutPage
